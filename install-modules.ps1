7601db469e48dbc506c1a5d5b3b3095d4d7837c7
Miguel Juan <mjuan@fraedom.com>
2022-02-22 13:49:08 +1300

    init

---
+++ install-modules.ps1
@@ -0,0 +1,3 @@
+Install-Module Fraedom.ReleaseManifest, JiraPS, CredentialManager, Posh-SSH -Repository ps -Scope CurrentUser
+Import-Module Fraedom.ReleaseManifest, JiraPS, CredentialManager, Posh-SSH
+Get-Help Get-Release
